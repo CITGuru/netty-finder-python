0a99fe7ba312824baf81fea9d3c579cce5209d5f
Oyetoke Toby <oyetoketoby80@gmail.com>
2018-11-12 07:44:56 +0100

    Rename readme to README for PyPI

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = path.abspath(path.dirname(__file__))
 
 # long description from the README file
 
-with open(path.join(here, 'readme.md'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
